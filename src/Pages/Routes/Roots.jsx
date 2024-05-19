98304f0493fe3b7fa7b3f74e55ac5d4e27cc6580
TI Sabbir <tisabbir0@gmail.com>
2024-05-19 19:58:57 +0600

    footer section added

--- src/Pages/Routes/Roots.jsx
+++ src/Pages/Routes/Roots.jsx
@@ -1,4 +1,5 @@
 import { Outlet } from "react-router-dom";
+import Footer from "../Shared/Footer/Footer";
 
 
 const Roots = () => {
@@ -6,6 +7,7 @@ const Roots = () => {
         <div>
             This is root
             <Outlet />
+            <Footer />
         </div>
     );
 };
