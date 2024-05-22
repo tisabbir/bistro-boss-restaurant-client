a466c42d4234659b5146a15c74a91162f08070c7
TI Sabbir <tisabbir0@gmail.com>
2024-05-22 13:07:31 +0600

    call us section created

---
+++ src/Pages/Home/CallUs/CallUs.jsx
@@ -0,0 +1,13 @@
+
+
+const CallUs = () => {
+    return (
+       <div className="px-4 my-6">
+         <div className="bg-[#151515] w-full py-24">
+            <h1 className="text-5xl font-semibold text-white text-center">Call Us : +8801812345678</h1>
+        </div>
+       </div>
+    );
+};
+
+export default CallUs;
--- src/Pages/Home/Home/Home.jsx
+++ src/Pages/Home/Home/Home.jsx
@@ -1,4 +1,5 @@
 import Banner from "../Banner/Banner";
+import CallUs from "../CallUs/CallUs";
 import Category from "../Category/Category";
 
 
@@ -7,6 +8,7 @@ const Home = () => {
         <div>
             <Banner />
             <Category />
+            <CallUs />
         </div>
     );
 };
