f41d214efc1b6da5083e3dc9c715a2048d8e0376
TI Sabbir <tisabbir0@gmail.com>
2024-05-23 06:29:49 +0600

    parallax effect on featured section added

--- src/Pages/Home/Featured/Featured.jsx
+++ src/Pages/Home/Featured/Featured.jsx
@@ -3,15 +3,15 @@ import featuredImg from '../../../../public/assets/home/featured.jpg'
 
 const Featured = () => {
     return (
-        <div className="">
+        <div className="bg-featured-image bg-fixed text-white py-12">
             <SectionTitles headings={'From Our Menu'} subheadings={'Check it out'} />
-            <div className="flex mt-12 mb-12 gap-6 items-center justify-center">
+            <div className="flex mt-12 mb-12 gap-6 items-center justify-center bg-black bg-opacity-40">
             <img className="w-[500px] h-[400px]" src={featuredImg} alt="" />
-            <div className="space-y-4">
+            <div className="space-y-4 ">
                 <h1>March 30, 2024</h1>
                 <h2>Where can I get some? </h2>
                 <p className="max-w-[550px]">Lorem ipsum dolor sit amet consectetur adipisicing elit. Esse pariatur soluta blanditiis perferendis quaerat repellendus dolorum odio. Perspiciatis praesentium veniam ipsum quibusdam optio facilis! Mollitia qui reiciendis amet tempora repudiandae.</p>
-                <button className="btn btn-outline border-0 border-b-2">Read More</button>
+                <button className="btn btn-outline border-0 border-b-2 text-white font-bold text-xl">Read More</button>
             </div>
             </div>
         </div>
--- tailwind.config.js
+++ tailwind.config.js
@@ -5,7 +5,12 @@ export default {
     "./src/**/*.{js,ts,jsx,tsx}",
   ],
   theme: {
-    extend: {},
+    extend: {
+      backgroundImage: {
+        'featured-image': "url('./public/assets/home/featured.jpg')",
+      },
+    },
+    
   },
   plugins: [
     require('daisyui'),
