55bfc09bb1b5fa7711185c28e5c9207d6b472e8a
TI Sabbir <tisabbir0@gmail.com>
2024-05-23 20:13:57 +0600

    all the data according to category loaded

--- src/Pages/OurShop/Tab/TabForm.jsx
+++ src/Pages/OurShop/Tab/TabForm.jsx
@@ -1,9 +1,19 @@
 import { useState } from "react";
 import { Tab, TabList, TabPanel, Tabs } from "react-tabs";
 import "react-tabs/style/react-tabs.css";
+import useMenu from "../../Hooks/useMenu";
+import FoodCard from "../../../Components/FoodCard/FoodCard";
 
 const TabForm = () => {
+  const [menu] = useMenu();
+  const salad = menu.filter((item) => item.category === "salad");
+  const pizza = menu.filter((item) => item.category === "pizza");
+  const soup = menu.filter((item) => item.category === "soup");
+  const dessert = menu.filter((item) => item.category === "dessert");
+  const drinks = menu.filter((item) => item.category === "drinks");
   const [tabIndex, setTabIndex] = useState(0);
+  console.log(salad, pizza, soup, dessert, drinks);
+  
   return (
     <div className="my-12 max-w-5xl mx-auto">
       <Tabs selectedIndex={tabIndex} onSelect={(index) => setTabIndex(index)}>
@@ -12,7 +22,7 @@ const TabForm = () => {
           <Tab>Pizza</Tab>
           <Tab>Soup</Tab>
           <Tab>Dessert</Tab>
-          <Tab>Drink</Tab>
+          <Tab>Drinks</Tab>
         </TabList>
         <TabPanel></TabPanel>
         <TabPanel></TabPanel>
@@ -20,6 +30,8 @@ const TabForm = () => {
         <TabPanel></TabPanel>
         <TabPanel></TabPanel>
       </Tabs>
+
+      <FoodCard />
     </div>
   );
 };
