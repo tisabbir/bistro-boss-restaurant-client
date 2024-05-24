c9b9d763fdf0b3d944ce83502a36696c67a60801
TI Sabbir <tisabbir0@gmail.com>
2024-05-24 22:12:24 +0600

    card design made as figma file

--- src/Components/FoodCard/FoodCard.jsx
+++ src/Components/FoodCard/FoodCard.jsx
@@ -1,5 +1,5 @@
 const FoodCard = ({item}) => {
-  const {name, recipe, category, price} = item;
+  const {name, recipe,  price} = item;
   return (
     <div className="card w-96 bg-base-100 shadow-xl">
       <figure>
@@ -8,12 +8,13 @@ const FoodCard = ({item}) => {
           alt="Shoes"
         />
       </figure>
+      <p className="text-white text-xl bg-slate-800 px-2 rounded-md absolute right-0 mt-4 mr-4">${price}</p>
       <div className="card-body">
         <h2 className="card-title">{name}</h2>
         <p>{recipe}</p>
-        <div className="card-actions justify-end">
-          <button className="btn btn-outline">{category}</button>
-          <button className="btn btn-outline">${price}</button>
+        <div className="card-actions justify-center">
+          
+          <button className="btn btn-outline uppercase border-0 border-b-4 bg-base-300">Add To Cart</button>
         </div>
       </div>
     </div>
