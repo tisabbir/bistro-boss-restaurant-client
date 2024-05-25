437cd663364162daa59cf09dc6b51af4142412df
TI Sabbir <tisabbir0@gmail.com>
2024-05-25 12:01:44 +0600

    now the data are loaded from mongodb

--- src/Pages/Hooks/useMenu.jsx
+++ src/Pages/Hooks/useMenu.jsx
@@ -4,7 +4,7 @@ import { useEffect, useState } from "react";
 const useMenu = () => {
     const [menu, setMenu] = useState([]);
     useEffect(()=>{
-        fetch('menu.json')
+        fetch('http://localhost:5000/menu')
         .then(res => res.json())
         .then(data => {
             setMenu(data)
