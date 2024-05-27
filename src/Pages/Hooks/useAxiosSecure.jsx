b0665a9c8a67179a0474a378ac8b8191e23cf38a
TI Sabbir <tisabbir0@gmail.com>
2024-05-27 21:20:36 +0600

    useAxiosSecure and useCart is added

--- src/Pages/Hooks/useAxiosSecure.jsx
+++ src/Pages/Hooks/useAxiosSecure.jsx
@@ -1,8 +1,22 @@
+// import axios from "axios";
+
 import axios from "axios";
 
+// const axiosSecure = axios.create({
+//     baseURL: 'http://localhost:5000'
+// })
+
+// const useAxiosSecure = () => {
+//     return axiosSecure;
+// };
+
+// export default useAxiosSecure;
+
+
 const axiosSecure = axios.create({
-    baseURL: 'http://localhost:5000'
+    baseURL : 'http://localhost:5000',
 })
+
 
 const useAxiosSecure = () => {
     return axiosSecure;
--- src/Pages/Hooks/useCart.jsx
+++ src/Pages/Hooks/useCart.jsx
@@ -1,21 +1,39 @@
+// import { useQuery } from "@tanstack/react-query";
+// import useAxiosSecure from "./useAxiosSecure";
+
 import { useQuery } from "@tanstack/react-query";
 import useAxiosSecure from "./useAxiosSecure";
 
 
-const useCart = () => {
+// const useCart = () => {
 
+//     const axiosSecure = useAxiosSecure();
+//     //TODO: use tan stack query
+//     const {data : cart = []} = useQuery({
+//         queryKey: ['cart'],
+//         queryFn: async () => {
+//             const res = await axiosSecure.get('/carts')
+//             return res.data;
+//         }
+//     })
+
+//     return [cart]
+    
+// };
+
+// export default useCart;
+
+const useCart = () => {
     const axiosSecure = useAxiosSecure();
-    //TODO: use tan stack query
     const {data : cart = []} = useQuery({
         queryKey: ['cart'],
-        queryFn: async () => {
+        queryFn: async() =>{
             const res = await axiosSecure.get('/carts')
             return res.data;
-        }
+        }   
     })
 
     return [cart]
-    
 };
 
 export default useCart;
