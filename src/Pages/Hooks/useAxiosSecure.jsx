17759d8aa70c5218d4b6cb6ee3d4ecf74613f4ea
TI Sabbir <tisabbir0@gmail.com>
2024-05-28 22:35:22 +0600

    if the token is not valid we give the user kickout

--- src/Pages/Hooks/useAxiosSecure.jsx
+++ src/Pages/Hooks/useAxiosSecure.jsx
@@ -1,6 +1,8 @@
 // import axios from "axios";
 
 import axios from "axios";
+import useAuth from "./useAuth";
+import { useNavigate } from "react-router-dom";
 
 // const axiosSecure = axios.create({
 //     baseURL: 'http://localhost:5000'
@@ -19,6 +21,39 @@ const axiosSecure = axios.create({
 
 
 const useAxiosSecure = () => {
+
+    const {logOut} = useAuth();
+    const navigate = useNavigate();
+    //request interceptor to add authorization headers for every secure api call
+    axiosSecure.interceptors.request.use(function(config) {
+        // console.log('request stopped by interceptors');
+        const token = localStorage.getItem('access-token');
+        // console.log('token from interceptor',token);
+        config.headers.authorization = `Bearer ${token}` //authorization spelling is important
+        return config;
+    }, function(error){
+        return Promise.reject(error)
+    })
+
+
+    //intercept 401 and 403 status
+    axiosSecure.interceptors.response.use(function(response){
+        return response;
+    }, function(error){
+        const status = error.response.status;
+        console.log('status error in interceptor', status);
+        if(status === 401 || status === 403){
+            logOut()
+            .then(()=>{})
+            .catch(err => {
+                console.log(err);
+            });
+
+            navigate('/login')
+
+        }
+        return Promise.reject(error)
+    })
     return axiosSecure;
 };
 
