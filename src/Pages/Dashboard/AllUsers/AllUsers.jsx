80101c89018608be60d6bc8daedda3d8e2fa6c16
TI Sabbir <tisabbir0@gmail.com>
2024-05-28 17:35:42 +0600

    user can be deleted now

--- src/Pages/Dashboard/AllUsers/AllUsers.jsx
+++ src/Pages/Dashboard/AllUsers/AllUsers.jsx
@@ -1,80 +1,88 @@
 import { useQuery } from "@tanstack/react-query";
 import useAxiosSecure from "../../Hooks/useAxiosSecure";
-
+import { FaTrash, FaUsers } from "react-icons/fa6";
+import Swal from "sweetalert2";
 
 const AllUsers = () => {
-    const axiosSecure = useAxiosSecure();
-    const {data : users = []} = useQuery({
-        queryKey: ['users'],
-        queryFn: async()=>{
-          const res = await  axiosSecure.get('/users')
-        return res.data;
+  const axiosSecure = useAxiosSecure();
+  const { data: users = [], refetch } = useQuery({
+    queryKey: ["users"],
+    queryFn: async () => {
+      const res = await axiosSecure.get("/users");
+      return res.data;
+    },
+  });
+
+  const handleDeleteUser = (id) => {
+    Swal.fire({
+        title: "Are you sure?",
+        text: "You won't be able to revert this!",
+        icon: "warning",
+        showCancelButton: true,
+        confirmButtonColor: "#3085d6",
+        cancelButtonColor: "#d33",
+        confirmButtonText: "Yes, delete it!"
+      }).then((result) => {
+        if (result.isConfirmed) {
+          axiosSecure.delete(`/users/${id}`)
+          .then(res => {
+            if(res.data.deletedCount > 0){
+                refetch();
+                Swal.fire({
+                    title: "Deleted!",
+                    text: "Your file has been deleted.",
+                    icon: "success"
+                  });
+            }
+          })
         }
-    })
-    return (
-        <div className="mt-12">
-            <div className="flex justify-evenly">
-                <h2 className="text-4xl">All Users</h2>
-                <h2 className="text-4xl">Total Users : {users.length}</h2>
-            </div>
-            <div>
-            <div className="overflow-x-auto">
-  <table className="table">
-    {/* head */}
-    <thead>
-      <tr>
-        <th>
-          <label>
-            <input type="checkbox" className="checkbox" />
-          </label>
-        </th>
-        <th>Name</th>
-        <th>Job</th>
-        <th>Favorite Color</th>
-        <th></th>
-      </tr>
-    </thead>
-    <tbody>
-      {/* row 1 */}
-      {
-        users.map(item => <tr key={item._id}>
-            <th>
-              <label>
-                <input type="checkbox" className="checkbox" />
-              </label>
-            </th>
-            <td>
-              <div className="flex items-center gap-3">
-                <div className="avatar">
-                  <div className="mask mask-squircle w-12 h-12">
-                    <img src="https://img.daisyui.com/tailwind-css-component-profile-2@56w.png" alt="Avatar Tailwind CSS Component" />
-                  </div>
-                </div>
-                <div>
-                  <div className="font-bold">Hart Hagerty</div>
-                  <div className="text-sm opacity-50">United States</div>
-                </div>
-              </div>
-            </td>
-            <td>
-              Zemlak, Daniel and Leannon
-              <br/>
-              <span className="badge badge-ghost badge-sm">Desktop Support Technician</span>
-            </td>
-            <td>Purple</td>
-            <th>
-              <button className="btn btn-ghost btn-xs">details</button>
-            </th>
-          </tr>)
-      }
-    </tbody>
-    
-    
-  </table>
-</div>
-            </div>
+      });
+    //
+  }
+  return (
+    <div className="mt-12">
+      <div className="flex justify-evenly">
+        <h2 className="text-4xl">All Users</h2>
+        <h2 className="text-4xl">Total Users : {users.length}</h2>
+      </div>
+      <div className="mt-12">
+        <div className="overflow-x-auto">
+          <table className="table">
+            {/* head */}
+            <thead>
+              <tr>
+                <th></th>
+                <th>Name</th>
+                <th>Email</th>
+                <th>Role</th>
+                <th>Action</th>
+              </tr>
+            </thead>
+            <tbody>
+              {/* row 1 */}
+              {users.map((item, index) => (
+                <tr key={item._id}>
+                  <th> {index + 1}</th>
+                  <td>{item.name}</td>
+                  <td>{item.email}</td>
+                  <th>
+                    <button className="btn btn-warning">
+                      <FaUsers />
+                    </button>
+                  </th>
+                  <th>
+                    <button onClick={()=>handleDeleteUser(item._id)} className="btn btn-error">
+                      <FaTrash />
+                    </button>
+                  </th>
+                </tr>
+              ))}
+            </tbody>
+          </table>
         </div>
-    );
+      </div>
+    </div>
+  );
 };
 
 export default AllUsers;
