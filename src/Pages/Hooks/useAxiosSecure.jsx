11a412168cba583f2c4568eca6c1995dce476487
TI Sabbir <tisabbir0@gmail.com>
2024-05-29 22:56:27 +0600

    logOut made async Await

--- src/Pages/Hooks/useAxiosSecure.jsx
+++ src/Pages/Hooks/useAxiosSecure.jsx
@@ -14,47 +14,48 @@ import { useNavigate } from "react-router-dom";
 
 // export default useAxiosSecure;
 
-
 const axiosSecure = axios.create({
-    baseURL : 'http://localhost:5000',
-})
-
+  baseURL: "http://localhost:5000",
+});
 
 const useAxiosSecure = () => {
+  const { logOut } = useAuth();
+  const navigate = useNavigate();
+  //request interceptor to add authorization headers for every secure api call
+  axiosSecure.interceptors.request.use(
+    function (config) {
+      // console.log('request stopped by interceptors');
+      const token = localStorage.getItem("access-token");
+      // console.log('token from interceptor',token);
+      config.headers.authorization = `Bearer ${token}`; //authorization spelling is important
+      return config;
+    },
+    function (error) {
+      return Promise.reject(error);
+    }
+  );
 
-    const {logOut} = useAuth();
-    const navigate = useNavigate();
-    //request interceptor to add authorization headers for every secure api call
-    axiosSecure.interceptors.request.use(function(config) {
-        // console.log('request stopped by interceptors');
-        const token = localStorage.getItem('access-token');
-        // console.log('token from interceptor',token);
-        config.headers.authorization = `Bearer ${token}` //authorization spelling is important
-        return config;
-    }, function(error){
-        return Promise.reject(error)
-    })
+  //intercept 401 and 403 status
+  axiosSecure.interceptors.response.use(
+    function (response) {
+      return response;
+    },
+    async (error) => {
+      const status = error.response.status;
+      console.log("status error in interceptor", status);
+      if (status === 401 || status === 403) {
+        await logOut()
+          .then(() => {})
+          .catch((err) => {
+            console.log(err);
+          });
 
-
-    //intercept 401 and 403 status
-    axiosSecure.interceptors.response.use(function(response){
-        return response;
-    }, function(error){
-        const status = error.response.status;
-        console.log('status error in interceptor', status);
-        if(status === 401 || status === 403){
-            logOut()
-            .then(()=>{})
-            .catch(err => {
-                console.log(err);
-            });
-
-            navigate('/login')
-
-        }
-        return Promise.reject(error)
-    })
-    return axiosSecure;
+        navigate("/login");
+      }
+      return Promise.reject(error);
+    }
+  );
+  return axiosSecure;
 };
 
 export default useAxiosSecure;
