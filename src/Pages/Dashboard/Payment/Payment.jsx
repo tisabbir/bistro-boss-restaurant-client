f8134281cc5282523b3e483adb7e5bc350c4cb5f
TI Sabbir <tisabbir0@gmail.com>
2024-06-02 23:40:17 +0600

    payment page created

---
+++ src/Pages/Dashboard/Payment/Payment.jsx
@@ -0,0 +1,12 @@
+import SectionTitles from "../../../Components/SectionTitles/SectionTitles";
+
+
+const Payment = () => {
+    return (
+        <div>
+            <SectionTitles headings={"Payment"} subheadings={"Please pay for your foods"} />
+        </div>
+    );
+};
+
+export default Payment;
